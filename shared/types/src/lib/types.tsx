20b153ed42831e9d77fce6edf4364224044e85f1
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-03-06 01:36:34 +0900

    inital commit

---
+++ shared/types/src/lib/types.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface TypesProps {}
+
+export function Types(props: TypesProps) {
+  return (
+    <div>
+      <h1>Welcome to Types!</h1>
+    </div>
+  );
+}
+
+export default Types;
